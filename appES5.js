15fb755c9459dd0845bba15f73a188687dcad8c4
bsralkan <bsr.alkan@gmail.com>
2020-09-23 14:05:29 +0300

    Online Courses project with JavaScript ES5

---
+++ appES5.js
@@ -0,0 +1,92 @@
+// Course Constructor
+function Course(title, instructor, image) {
+    this.title = title;
+    this.instructor = instructor;
+    this.image = image;
+}
+
+// UI Constructor
+function UI() {
+
+}
+
+UI.prototype.addCourseToList = function(course) {
+    const list = document.getElementById('course-list');
+
+    var html = `
+        <tr>
+            <td><img src="img/${course.image}" /></td>
+            <td>${course.title}</td>
+            <td>${course.instructor}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+        </tr>
+    `;
+    list.innerHTML += html;
+}
+
+UI.prototype.clearControls = function() {
+    const title = document.getElementById('title').value = "";
+    const instructor = document.getElementById('instructor').value = "";
+    const image = document.getElementById('image').value = "";
+}
+
+UI.prototype.deleteCourse = function(element) {
+    if (element.classList.contains('delete')) {
+        //delete butonunun bulunduğu yerin parent'ı td bir önceki parent td olduğu için td yi silmeye çalışıyoruz.
+        element.parentElement.parentElement.remove();
+    }
+}
+
+UI.prototype.showAlert = function(message, className) {
+    var alert = `
+        <div class="alert alert-${className}">
+            ${message}
+        </div>
+    `;
+
+    const row = document.querySelector('.row');
+
+    // insertAdjacentHTML fonksiyonunun beklediği parametreler beforeBegin, afterBegin, beforeEnd, afterEnd
+    row.insertAdjacentHTML('beforeBegin', alert);
+
+    // verilen alertin belli saniye sonra silinmesi için
+    setTimeout(() => {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
+document.getElementById('new-course').addEventListener('submit', function(e) {
+
+    const title = document.getElementById('title').value;
+    const instructor = document.getElementById('instructor').value;
+    const image = document.getElementById('image').value;
+
+    // Course object
+    const course = new Course(title, instructor, image);
+
+    // UI object
+    const ui = new UI();
+
+    if (title === '' || instructor === '' || image === '') {
+        ui.showAlert('please complete the form', 'warning');
+    } else {
+        //add course list
+        ui.addCourseToList(course);
+
+        //clear controls
+        ui.clearControls();
+
+        ui.showAlert('the course has been added', 'success');
+    }
+
+
+
+
+    e.preventDefault();
+});
+
+document.getElementById('course-list').addEventListener('click', function(e) {
+    const ui = new UI();
+    ui.deleteCourse(e.target);
+    ui.showAlert('the course has been deleted', 'danger');
+})
